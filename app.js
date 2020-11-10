7bfcecf259fd9fa2ef284b3982eb42d813be7510
Mark McCoid <markmccoid@gmail.com>
2020-11-10 10:04:02 -0500

    set status and header

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/demo', (req,res) => {
+	res.set('X-full-stack', 'customer header data');
+	res.status(418);
+	res.send('418---Result');
+});
+
 app.listen(port, () => console.log('Example app listening on port ${port}'));
